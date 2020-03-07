5dc1e876b4be2c6b130787b8397ee59d014d148f
heyrict <xiezh0831@yahoo.co.jp>
2020-03-07 13:40:54 +0800

    feat(convert_json): Add fallback parameter

--- convert_json.py
+++ convert_json.py
@@ -3,6 +3,22 @@ import json
 
 from lib import bitdecode_answer
 
+
+def gen_script(matrix):
+    return f"""
+function get_urlparam(key) {{
+  re = new RegExp(`${{key}}=([^&]+)`);
+  match = re.exec(window.location.search);
+  return match && match[1];
+}}
+
+esid = get_urlparam("id");
+data = JSON.parse('{json.dumps(matrix, ensure_ascii=False)}');
+data.forEach(([key, value]) => storage.setItem(esid + key, value));
+window.location.reload();
+"""
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('exhaust_file', help="Input json file")
@@ -10,12 +26,14 @@ if __name__ == "__main__":
                         '--output',
                         default=None,
                         help="Output javascript file")
-    parser.add_argument('-I',
-                        '--id',
-                        default=None,
-                        help="Id in url query parameter")
+    parser.add_argument(
+        '-f',
+        '--fall-back',
+        default=False,
+        action='store_true',
+        help="Use user's selection but fall back to true answer")
     parser.add_argument('-T',
-                        '--true_answer',
+                        '--true-answer',
                         default=False,
                         action='store_true',
                         help="Use true answer")
@@ -24,32 +42,26 @@ if __name__ == "__main__":
     with open(args.exhaust_file) as f:
         data = json.load(f)
 
-        if args.id:
-            esid = args.id
-        else:
-            esid = data.get('esid')
-
-        if esid is None:
-            raise Exception(
-                "Exam Student ID must be provided."
-                "either with id option, or provided in the input file")
-
         if args.true_answer:
             matrix = [[
-                f"{args.id}{q['id']}",
+                f"{q['id']}",
                 ','.join(list(q['answer'])),
             ] for q in data['questions'] if q['answer'] != None]
+        elif args.fall_back:
+            matrix = [[
+                f"{q['id']}",
+                ','.join(list(bitdecode_answer(q['user_selection']))),
+            ] if q['user_selection'] != 0 else [
+                f"{q['id']}",
+                ','.join(list(q['answer'])),
+            ] for q in data['questions']]
         else:
             matrix = [[
-                f"{args.id}{q['id']}",
+                f"{q['id']}",
                 ','.join(list(bitdecode_answer(q['user_selection']))),
             ] for q in data['questions'] if q['user_selection'] != 0]
 
-        script = f"""
-        data = JSON.parse(`{json.dumps(matrix, ensure_ascii=False)}`);
-        data.forEach(([key, value]) => storage.setItem(key, value));
-        window.location.reload();
-        """
+        script = gen_script(matrix)
 
     if args.output:
         with open(args.output, 'w') as f:
